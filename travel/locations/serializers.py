a937ec63f211b016d9da1d0f9a4aa5ee9c1b4835
XetPy1030 <xetpy1030@mail.ru>
2025-04-30 00:26:35 +0300

    Добавлен тип

--- travel/locations/serializers.py
+++ travel/locations/serializers.py
@@ -1,3 +1,5 @@
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import extend_schema_field
 from rest_framework import serializers
 
 from travel.locations.models import District, Settlement
@@ -5,6 +7,10 @@ from travel.locations.models import District, Settlement
 
 class SettlementSerializer(serializers.ModelSerializer):
     type_display = serializers.CharField(source='get_type_display', read_only=True)
+
+    @extend_schema_field(OpenApiTypes.BOOL)
+    def get_is_city_district(self, obj):
+        return obj.is_city_district
 
     class Meta:
         model = Settlement
