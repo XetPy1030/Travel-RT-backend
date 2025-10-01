c747b399fbb80f40b772bfdf76d3e0c95024d01d
XetPy1030 <xetpy1030@mail.ru>
2025-10-01 11:46:34 +0300

    Настройка для прода

--- travel/settings/prod.py
+++ travel/settings/prod.py
@@ -7,3 +7,5 @@ DEBUG = os.getenv('DEBUG', 'False').lower() in ('true', '1', 'yes', 'on')
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
 CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS').split(',')
+
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS').split(',')
